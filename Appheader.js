8f045ec8756d6916ef09b3fc914013664e4d58cf
Chaitawat1 <71750868+Chaitawat1@users.noreply.github.com>
2021-02-17 12:31:05 +0700

    Add files via upload

---
+++ Appheader.js
@@ -0,0 +1,12 @@
+function Appheader() {
+
+return (
+    <header className="App">
+      <img className="App-header" src = "/pic/12.jpg"/>
+        
+
+
+      </header>
+        );
+    }
+  export default Appheader;
---
+++ items.js
@@ -0,0 +1,23 @@
+const items =[
+    {
+      title :"หมา",
+      thumbnaiUrl :"/pic/1.jpg"
+    },
+
+    {
+      title :"หมา",
+      thumbnaiUrl :"/pic/11.jpg"
+    },
+
+    {
+      title :"หมา",
+      thumbnaiUrl :"/pic/111.jpg"
+    },
+
+    {
+      title :"หมา",
+      thumbnaiUrl :"/pic/1111.jpg"
+    }
+    
+  ];
+  export default items;
